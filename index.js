8b3a983b8cd383b8a21d54d331544b284e6336e9
DanielMasgrau <daniel.masgrau@enti.cat>
2025-01-21 20:10:25 +0100

    ws+syncBallAndPlayerMovment

---
+++ index.js
@@ -0,0 +1,82 @@
+const PORT = 7777;
+
+let http = require('http');
+let static = require('node-static');
+let ws = require('ws'); 
+
+
+//
+// Create a node-static server instance to serve the './public' folder
+//
+let file = new static.Server('./public');
+ 
+let http_server = http.createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(PORT);
+
+
+let ws_server = new ws.Server({server: http_server});
+let player1, player2;
+let ball;
+ws_server.on('connection', function (conn) {
+	console.log("Usuario conectado");
+
+	if (player1 == null){		
+		player1 = conn;
+		
+			let info = {
+			player_num: 1
+		};
+
+		player1.send(JSON.stringify(info) );
+	
+		player1.on('message', function (msg) {
+		console.log("Jugador 1: "+msg);
+		if (player2 == null)
+			return;
+
+		let info = JSON.parse(msg);
+		if (info.y != null) {
+			player2.send( JSON.stringify(info));
+		}
+		if (info.by != null) {
+			player2.send( JSON.stringify(info) );
+		}
+		else if(info.score1 != null || info.score2 != null) {
+			player2.send( JSON.stringify(info) );
+		}
+	});
+
+}
+
+	else if (player2 == null) {
+		player2 = conn;
+	
+		let info = {
+			player_num: 2
+		};
+
+		player2.send(JSON.stringify(info) );
+	
+		
+		player2.on('message', function (msg) {
+		console.log("Jugador 2: "+msg);
+		if (player1 == null)
+			return;
+
+		let info = JSON.parse(msg);
+		if (info.y != null) {
+			player1.send( JSON.stringify(info));
+		
+		console.log("Jugador 2: "+msg);
+
+		}
+
+	});
+}
+});
